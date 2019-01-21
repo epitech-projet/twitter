6dc932b3a0f8da0fabd1b85e29909c3a7c5f619c
Marjorie Adolphi <marjorie.adolphi@epitech.eu>
2019-01-21 17:02:10 +0100

    Marjorie

---
+++ Marjorie/Marjo_twitter.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="Marjo_twitter.css" />
+    <title>My_Twitter</title>
+</head>
+<body>
+    <header>
+        <h1>My_Twitter</h1> 
+    </header>
+   
+</body>
+</html>
